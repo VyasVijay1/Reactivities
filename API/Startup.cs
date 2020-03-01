d877c9deae13c318f61da0e9e4a922da0e23528a
VyasVijay1 <learning.curve.42@gmail.com>
2020-03-01 01:07:49 -0700

    added semantic-ui-react

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,13 @@ namespace API
 			{
 				opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
 			});
+			services.AddCors(opt =>
+			{
+				opt.AddPolicy("CorsPolicy", policy =>
+				{
+					policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+				});
+			});
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
 
@@ -46,6 +53,7 @@ namespace API
 				app.UseHsts();
 			}
 
+			app.UseCors("CorsPolicy");
 			app.UseHttpsRedirection();
 			app.UseMvc();
 		}

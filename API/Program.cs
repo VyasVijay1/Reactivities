70e10b1ff11017c4967d407e6ec97052ad12a322
VyasVijay1 <learning.curve.42@gmail.com>
2020-03-10 07:04:49 -0700

    CRUD application completed

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +22,7 @@ namespace API
 				{
 					var context = services.GetRequiredService<DataContext>();
 					context.Database.Migrate();
+					Seed.SeedData(context);
 				}
 				catch(Exception ex)
 				{
